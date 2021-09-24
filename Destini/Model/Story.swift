ce18bede22c6b893990805d466fe8dc3c8c0783e
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2021-09-24 15:01:17 +0300

    mvc design pattern. 2 blank model files. main storyboard with a choice ui and pretty look

---
+++ Destini/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Eetu Hernesniemi on 24.9.2021.
+//
+
+import Foundation
---
+++ Destini/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Destini
+//
+//  Created by Eetu Hernesniemi on 24.9.2021.
+//
+
+import Foundation
